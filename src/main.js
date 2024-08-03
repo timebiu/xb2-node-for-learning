3cd88949f21a40b467fea2d66ccae38ffcd39d96
Qiu <qiuzihao1128@gmail.com>
2024-08-03 09:50:29 +0800

    express test 1

--- src/main.js
+++ src/main.js
@@ -1,23 +1,44 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((request, response) => {
-    const data = {
-        id: 1,
-        title: '关山月',
-        content: '明月出天山，苍茫云海间',
-    }
-
-    const jsonData = JSON.stringify(data);
-
-    response.writeHead(200, {
-        'Content-Type': 'application/json; charset=utf-8'
-    });
-
-    response.write(jsonData);
-
-    response.end();
+app.listen(port, () => {
+    console.log('服务已启动！');
 });
 
-server.listen(3000, () => {
-    console.log('the server is on')
+app.get('/', (request, response) => {
+    response.send('你好');
+});
+
+const data = [
+    {
+        id: 1,
+        title: '关山月',
+        content: '明月出天山，苍茫云海间'
+    },
+    {
+        id: 2,
+        title: '望岳',
+        content: '会当凌绝顶，一览众山小'
+    },
+    {
+        id: 3,
+        title: '忆江南',
+        content: '日出江花红胜火，春来江水绿如蓝'
+    }
+];
+
+app.get('/posts', (request, response) => {
+    response.send(data);
+});
+
+app.get('/posts/:postId', (request, response) => {
+    // 获取内容ID
+    const { postId } = request.params;
+
+    // 查找具体内容
+    const posts = data.filter(item => item.id == postId);
+
+    //做出响应
+    response.send(posts[0]);
 });
