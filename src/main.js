50e2874f16ba851d47aae44015586e78a19455c9
InfiHeart <785783195@qq.com>
2024-08-04 22:09:46 +0800

    调试http

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+/**
+ * 使用JSON中间件
+ */
+app.use(express.json()); //不设置的话就是在全局使用中间件；作用是处理客户端发送的json数据，处理之后，接口的处理器就可以直接用里面的数据了
+
 app.listen(port, () => {
     console.log('服务已启动！');
 });
@@ -39,6 +45,28 @@ app.get('/posts/:postId', (request, response) => {
     // 查找具体内容
     const posts = data.filter(item => item.id == postId);
 
-    //做出响应
+    // 做出响应
     response.send(posts[0]);
+});
+
+/**
+ * 创建内容
+ */
+app.post('/posts', (request, response) => {
+    // 获取请求里的数据
+    const { content } = request.body;
+
+    // 设置响应状态码
+    response.status(201);
+
+    // 输出请求头部数据
+    console.log(request.headers['sing-along']);
+
+    // 设置响应的头部数据
+    response.set('Sing-along', 'How I wonder what you are!');
+
+    // 做出响应
+    response.send({
+        message: `成功创建了内容：${content}`
+    });
 });
