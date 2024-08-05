f333ca3d83d2a3b57bdcaa84562e844793e5ce6d
InfiHeart <785783195@qq.com>
2024-08-05 23:32:04 +0800

    代码开发体验优化： 1. 使用typescript语言开发js，可以使用js的新特性； 2. 添加tsc-watch库，能够自动重启服务； 3. 添加prettier库，能够自动格式化代码。

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-const express = require('express');
+import express from 'express';
+import { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
@@ -13,7 +14,7 @@ app.listen(port, () => {
 });
 
 app.get('/', (request, response) => {
-    response.send('你好');
+    response.send('你好~');
 });
 
 const data = [
@@ -34,16 +35,16 @@ const data = [
     }
 ];
 
-app.get('/posts', (request, response) => {
+app.get('/posts', (request: Request, response: Response) => {
     response.send(data);
 });
 
-app.get('/posts/:postId', (request, response) => {
+app.get('/posts/:postId', (request: Request, response: Response) => {
     // 获取内容ID
     const { postId } = request.params;
 
     // 查找具体内容
-    const posts = data.filter(item => item.id == postId);
+    const posts = data.filter(item => item.id == parseInt(postId));
 
     // 做出响应
     response.send(posts[0]);
@@ -52,7 +53,7 @@ app.get('/posts/:postId', (request, response) => {
 /**
  * 创建内容
  */
-app.post('/posts', (request, response) => {
+app.post('/posts', (request: Request, response: Response) => {
     // 获取请求里的数据
     const { content } = request.body;
 
