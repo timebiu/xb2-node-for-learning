036005125101126526e38c4bd61d6e12230f2e12
InfiHeart <785783195@qq.com>
2024-08-17 23:26:36 +0800

    创建用户

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,3 +1,23 @@
 import {Request, Response, NextFunction} from 'express';
 import { UserModel } from './user.model';
 import * as UserService from './user.service';
+
+/**
+ * 创建用户
+ */
+export const store = async (
+    requset: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    // 准备数据
+    const {name, password} = requset.body;
+
+    // 创建用户
+    try {
+        const data = await UserService.createUser({name, password});
+        response.status(201).send(data);
+    }catch(error){
+        next(error);
+    }
+}
