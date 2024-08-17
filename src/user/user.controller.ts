185b5186ee6b0a44867aa82875368aa96c8d9165
InfiHeart <785783195@qq.com>
2024-08-17 23:12:59 +0800

    初始化user相关的文件

---
+++ src/user/user.controller.ts
@@ -0,0 +1,3 @@
+import {Request, Response, NextFunction} from 'express';
+import { UserModel } from './user.model';
+import * as UserService from './user.service';
---
+++ src/user/user.middleware.ts
@@ -0,0 +1,2 @@
+import {Request, Response, NextFunction} from 'express';
+import * as UserService from './user.service';
