60deca6338141b3cd34ee3e40c3cb6421868909b
InfiHeart <785783195@qq.com>
2024-08-18 10:42:04 +0800

    用户登录文件创建，简单登录功能实现

---
+++ src/auth/auth.router.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import * as authController from './auth.controller';
+
+const router: express.Router = express.Router();
+
+/**
+ * 用户登录
+ */
+router.post('/login', authController.login);
+
+/**
+ * 导出路由
+ */
+export default router;
