4e7ce6aface3ac1ad34fd82a6b03a0e9f943c340
InfiHeart <785783195@qq.com>
2025-01-05 21:48:01 +0800

    add: 记录上传文件信息，存储到数据库

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,1 +1,21 @@
 import { connection } from '../app/database/mysql';
+import { FileModel } from './file.model';
+
+/**
+* 存储文件信息
+*/
+export const createFile = async (
+    file: FileModel
+) => {
+    // 准备查询
+    const satement = `
+        INSERT INTO file
+        SET ?
+        `;
+
+    // 执行查询
+    const [data] = await connection.promise().query(satement, file);
+
+    // 提供数据
+    return data;
+};
