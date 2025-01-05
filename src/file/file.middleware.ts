256373f920612b29c9de8c55fbf5d3b6184ced89
InfiHeart <785783195@qq.com>
2025-01-05 21:27:26 +0800

    add: 完善文件上传与存储功能

--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -1,2 +1,14 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, RequestHandler } from "express";
 import multer from 'multer';
+
+/**
+ * 创建一个multer
+ */
+const fileUpload = multer({
+    dest: 'uploads/',
+});
+
+/**
+ * 文件拦截器
+ */
+export const fileInterceptor: RequestHandler = fileUpload.single('file');
--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import { Router } from 'express';
 import * as fileController from './file.controller';
+import { authGuard } from '../auth/auth.middleware';
+import { fileInterceptor } from './file.middleware';
 
 const router: Router = express.Router();
+
+/**
+ * 文件上传
+ */
+router.post('/files', authGuard, fileInterceptor, fileController.store);
 
 /**
  * 导出路由
